e3a7c85253a01bda0e51f7a669eb69bfa4ede870
Alex <abadragan@gmail.com>
2022-11-05 15:35:27 +0100

    cars assemble submitted

--- exercism/Cars Assemble/main.go
+++ exercism/Cars Assemble/main.go
@@ -2,18 +2,29 @@
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(CalculateWorkingCarsPerHour(1547, 90))
+	fmt.Println(CalculateWorkingCarsPerMinute(1105, 90))
+	fmt.Println(CalculateCost(37))
+	fmt.Println(CalculateCost(21))
 }
 
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	panic("CalculateWorkingCarsPerHour not implemented")
+	// workingCarsPerHour := float64(productionRate) * successRate / 100
+	// return workingCarsPerHour
+	return float64(productionRate) * successRate / 100
 }
 
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	panic("CalculateWorkingCarsPerMinute not implemented")
+	// workingCarsPerMinute := float64(productionRate) * successRate / 100 / 60
+	// return int(workingCarsPerMinute)
+	return (int)(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
 }
 
 func CalculateCost(carsCount int) uint {
-	panic("CalculateCost not implemented")
+	tenGroup := carsCount / 10
+	remainingGroup := carsCount % 10
+	return uint(tenGroup)*95000 + uint(remainingGroup)*10000
 }
