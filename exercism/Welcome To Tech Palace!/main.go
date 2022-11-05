4c40a58fc276f4a3245367c5f692b48abc3b3d0b
Alex <abadragan@gmail.com>
2022-11-05 15:41:07 +0100

    Welcome To Tech Palace! initial commit

---
+++ exercism/Welcome To Tech Palace!/main.go
@@ -0,0 +1,19 @@
+// https://exercism.org/tracks/go/exercises/welcome-to-tech-palace
+
+package main
+
+func main() {
+
+}
+
+func WelcomeMessage(customer string) string {
+	panic("Please implement the WelcomeMessage() function")
+}
+
+func AddBorder(welcomeMsg string, numStarsPerLine int) string {
+	panic("Please implement the AddBorder() function")
+}
+
+func CleanupMessage(oldMsg string) string {
+	panic("Please implement the CleanupMessage() function")
+}
