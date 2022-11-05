4bae743418dccbdb7c9ffb2823a39d33060105c3
Alex <abadragan@gmail.com>
2022-11-05 15:04:24 +0100

    car assemble initial commit

---
+++ exercism/Cars Assemble/main.go
@@ -0,0 +1,19 @@
+// https://exercism.org/tracks/go/exercises/cars-assemble
+
+package main
+
+func main() {
+
+}
+
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	panic("CalculateWorkingCarsPerHour not implemented")
+}
+
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	panic("CalculateWorkingCarsPerMinute not implemented")
+}
+
+func CalculateCost(carsCount int) uint {
+	panic("CalculateCost not implemented")
+}
