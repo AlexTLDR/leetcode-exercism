29240d97b980213569a9a6de989f3285c563fa87
Alex <abadragan@gmail.com>
2022-11-06 14:27:04 +0100

    blackjack initial commit

---
+++ exercism/Blackjack/main.go
@@ -0,0 +1,18 @@
+// https://exercism.org/tracks/go/exercises/blackjack
+
+package main
+
+func main() {
+
+}
+
+// ParseCard returns the integer value of a card following blackjack ruleset.
+func ParseCard(card string) int {
+	panic("Please implement the ParseCard function")
+}
+
+// FirstTurn returns the decision for the first turn, given two cards of the
+// player and one card of the dealer.
+func FirstTurn(card1, card2, dealerCard string) string {
+	panic("Please implement the FirstTurn function")
+}
