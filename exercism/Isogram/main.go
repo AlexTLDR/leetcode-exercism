9c34cd47bcfffa601083b6d7391817c8a3acd585
Alex <abadragan@gmail.com>
2022-11-06 18:55:09 +0100

    work in progress

--- exercism/Isogram/main.go
+++ exercism/Isogram/main.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println(IsIsogram("lumberjacks"))
+	fmt.Println(IsIsogram("slumberjacks"))
 }
 
 func IsIsogram(word string) bool {
+	for i := len(word) - 1; i > 0; i-- {
+		for j := 1; j < i; j++ {
+			fmt.Println("i:", word[i], "j:", word[j])
+		}
+		return false
+	}
 	return true
 }
