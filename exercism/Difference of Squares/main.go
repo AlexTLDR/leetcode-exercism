47372a99bc8f2536c4c704290363690c3ed1dd2a
Alex <abadragan@gmail.com>
2022-11-09 15:19:08 +0100

    exercism test passed

--- exercism/Difference of Squares/main.go
+++ exercism/Difference of Squares/main.go
@@ -2,16 +2,37 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	fmt.Println(SquareOfSum(10))
+	fmt.Println(SumOfSquares(10))
+	fmt.Println(Difference(10))
 }
 
 func SquareOfSum(n int) int {
-	return 0
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+	return sum * sum
 }
 
 func SumOfSquares(n int) int {
-	return 0
+	sum := 0
+	for i := 0; i <= n; i++ {
+		square := SquareInt(i)
+		sum += square
+	}
+	return sum
+}
+
+func SquareInt(n int) int {
+	result := math.Pow(float64(n), 2)
+	return int(result)
 }
 
 func Difference(n int) int {
