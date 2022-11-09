fe5a0dca1a1d9763d4b1d794024e67376311077d
Alex <abadragan@gmail.com>
2022-11-09 15:27:14 +0100

    added the mathematical approach

--- exercism/Difference of Squares/main.go
+++ exercism/Difference of Squares/main.go
@@ -38,3 +38,32 @@ func SquareInt(n int) int {
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
+
+/*
+A better approach is to use Gauss' formula to calculate the sum of the numbers;
+
+	    n(n+1)
+	    ------
+	S =   2
+
+	and the square pyramidal number is the sum of the squares
+
+			n(n + 1)(2n + 1)
+			---------------
+	S = 			 6
+
+	Which yields O(1) time requirement and O(1) space requirement.
+
+
+func SquareOfSum(n int) int {
+	sum := (n * (n + 1)) / 2
+	return sum * sum
+}
+func SumOfSquares(n int) int {
+	return (n * (n + 1) * (2*n + 1)) / 6
+}
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
+
+*/
