1a286588647336f40f588fa7d4c88486d396cbe9
Alex <abadragan@gmail.com>
2022-11-09 13:12:50 +0100

    Difference of Squares initial commit

---
+++ exercism/Difference of Squares/main.go
@@ -0,0 +1,19 @@
+// https://exercism.org/tracks/go/exercises/difference-of-squares
+
+package main
+
+func main() {
+
+}
+
+func SquareOfSum(n int) int {
+	return 0
+}
+
+func SumOfSquares(n int) int {
+	return 0
+}
+
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
