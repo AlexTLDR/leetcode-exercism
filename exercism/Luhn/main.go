72efa3840bd95f963c073a4c4e47182243948b69
Alex <abadragan@gmail.com>
2022-11-09 17:31:43 +0100

    Luhn initial commit

---
+++ exercism/Luhn/main.go
@@ -0,0 +1,13 @@
+// https://exercism.org/tracks/go/exercises/luhn
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Valid("4539 3195 0343 6467"))
+}
+
+func Valid(id string) bool {
+	return false
+}
