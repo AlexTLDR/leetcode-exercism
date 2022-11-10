fa317713b04ad6cf033a0155f4e2f228dacb84f2
Alex <abadragan@gmail.com>
2022-11-10 13:21:12 +0100

    added doubleDigitValue func

--- exercism/Luhn/main.go
+++ exercism/Luhn/main.go
@@ -2,12 +2,35 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println(Valid("4539 3195 0343 6467"))
+	//fmt.Println(Valid("4539 3195 0343 6467"))
+	fmt.Println(Valid(strings.Replace("4539 3195 0343 6467", " ", "", -1)))
 }
 
 func Valid(id string) bool {
+	//strings.Replace(id, " ", "", -1)
+	var idArr []int
+	fmt.Println(id)
+	for _, v := range id {
+		idArr = append(idArr, int(v-48))
+	}
+
+	fmt.Println(idArr)
 	return false
+}
+
+func doubleDigitValue(digit int) (value int) {
+	doubleDigit := digit * 2
+
+	if doubleDigit > 9 {
+		value = doubleDigit - 9
+	} else {
+		value = doubleDigit
+	}
+	return value
 }
