176cd947f194ca1c567babbaa57a81f91667cdf1
Alex <abadragan@gmail.com>
2022-11-11 20:51:26 +0100

    refactored

--- exercism/Isogram/main.go
+++ exercism/Isogram/main.go
@@ -8,20 +8,25 @@ import (
 )
 
 func main() {
-	fmt.Println(IsIsogram("lumbserjacks"))
+	fmt.Println(IsIsogram("lumbee r ja-c-ks"))
 }
 
 func IsIsogram(word string) bool {
-	var wordMap map[rune]int = make(map[rune]int)
-	word = strings.Replace(word, "-", "", -1)
-	word = strings.Replace(word, " ", "", -1)
+	var wordMap map[rune]bool = make(map[rune]bool)
+	word = strings.ReplaceAll(word, "-", "")
+	word = strings.ReplaceAll(word, " ", "")
 	for _, v := range strings.ToLower(word) {
-		wordMap[v]++
-	}
-	for _, v := range wordMap {
-		if v > 1 {
+
+		if wordMap[v] == true {
 			return false
 		}
+		wordMap[v] = true
 	}
+	fmt.Println("from println:", wordMap)
+	// for _, v := range wordMap {
+	// 	if v > 1 {
+	// 		return false
+	// 	}
+	// }
 	return true
 }
