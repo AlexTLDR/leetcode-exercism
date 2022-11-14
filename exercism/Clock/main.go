910fb73347dd15bd21878425997036e52a64730b
Alex <abadragan@gmail.com>
2022-11-14 14:35:24 +0100

    clock initial commit

---
+++ exercism/Clock/main.go
@@ -0,0 +1,23 @@
+// https://exercism.org/tracks/go/exercises/grains
+
+package main
+
+func main() {
+
+}
+
+func New(h, m int) Clock {
+	panic("Please implement the New function")
+}
+
+func (c Clock) Add(m int) Clock {
+	panic("Please implement the Add function")
+}
+
+func (c Clock) Subtract(m int) Clock {
+	panic("Please implement the Subtract function")
+}
+
+func (c Clock) String() string {
+	panic("Please implement the String function")
+}
