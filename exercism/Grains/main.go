868a6a39fda9f4b98e0da1088189fdf692ca69eb
Alex <abadragan@gmail.com>
2022-11-14 13:50:16 +0100

    passed all the tests

--- exercism/Grains/main.go
+++ exercism/Grains/main.go
@@ -20,19 +20,5 @@ func Square(number int) (uint64, error) {
 }
 
 func Total() uint64 {
-	/* brute force
-	var chessBoard []uint64
-	total := 0.0
-	for i := 0; i < 64; i++ {
-		chessBoard = append(chessBoard, uint64(i))
-	}
-
-	for i, v := range chessBoard {
-		total += math.Pow(float64(v), float64(i))
-	}
-	return uint64(total)
-	*/
-
-	// Mathematical Solution
-	return uint64(math.Pow(2, 64) - 1)
+	return (1 << 64) - 1
 }
