d672e82ce2f0ab08dd58d961cda7214957bd0dcf
Alex <abadragan@gmail.com>
2022-11-14 12:01:44 +0100

    passed all the func Square tests

--- exercism/Grains/main.go
+++ exercism/Grains/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
 
 func main() {
 	fmt.Println(Total())
+	fmt.Println(Square(4))
 }
 
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	if number <= 0 || number > 64 {
+		err := errors.New("not a valid chess square")
+		return uint64(math.Pow(2, float64(number-1))), err
+	}
+	return uint64(math.Pow(2, float64(number-1))), nil
 }
 
 func Total() uint64 {
