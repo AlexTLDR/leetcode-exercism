35eb490916a5b9ed907df166a1474f7fe7495bfd
Alex <abadragan@gmail.com>
2022-11-14 11:37:41 +0100

    implemented func Total

--- exercism/Grains/main.go
+++ exercism/Grains/main.go
@@ -1,7 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	fmt.Println(Total())
 }
 
 func Square(number int) (uint64, error) {
@@ -9,5 +14,19 @@ func Square(number int) (uint64, error) {
 }
 
 func Total() uint64 {
-	panic("Please implement the Total function")
+	/* brute force
+	var chessBoard []uint64
+	total := 0.0
+	for i := 0; i < 64; i++ {
+		chessBoard = append(chessBoard, uint64(i))
+	}
+
+	for i, v := range chessBoard {
+		total += math.Pow(float64(v), float64(i))
+	}
+	return uint64(total)
+	*/
+
+	// Mathematical Solution
+	return uint64(math.Pow(2, 64) - 1)
 }
