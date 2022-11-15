28fc617d812bf075e9734906b632e0cdfd1ef072
Alex <abadragan@gmail.com>
2022-11-15 11:06:49 +0100

    Need For Speed initial commit

---
+++ exercism/Need For Speed/main.go
@@ -0,0 +1,29 @@
+// https://exercism.org/tracks/go/exercises/need-for-speed
+
+package main
+
+func main() {
+
+}
+
+func NewCar(speed, batteryDrain int) Car {
+	panic("Please implement the NewCar function")
+}
+
+// TODO: define the 'Track' type struct
+
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	panic("Please implement the NewTrack function")
+}
+
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+	panic("Please implement the Drive function")
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	panic("Please implement the CanFinish function")
+}
