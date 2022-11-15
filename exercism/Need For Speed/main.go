524bfe39d2ded514dee5b54589ab69c2bfb33eba
Alex <abadragan@gmail.com>
2022-11-15 11:39:06 +0100

    5 tests passed, 9 failed

--- exercism/Need For Speed/main.go
+++ exercism/Need For Speed/main.go
@@ -11,31 +11,45 @@ type Car struct {
 	distance     int
 }
 
+type Track struct {
+	distance int
+}
+
 func main() {
-	speed := 5
+	//Car.battery := 100
 	batteryDrain := 2
+	speed := 5
+	distance := 0
+
 	car := NewCar(speed, batteryDrain)
 	fmt.Println(car)
+
+	distance = 800
+	track := NewTrack(distance)
+	fmt.Println(track)
+
+	fmt.Println(Drive(car))
+	fmt.Println(CanFinish(car, track))
 }
 
 func NewCar(speed, batteryDrain int) Car {
-	return Car{speed: speed, batteryDrain: batteryDrain}
+	return Car{battery: 100, speed: speed, batteryDrain: batteryDrain, distance: speed}
 }
 
-// TODO: define the 'Track' type struct
+func NewTrack(distance int) Track {
+	return Track{distance}
+}
 
-// NewTrack creates a new track
-// func NewTrack(distance int) Track {
-// 	panic("Please implement the NewTrack function")
-// }
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+	return Car{battery: car.battery - car.batteryDrain, speed: car.speed}
+}
 
-// // Drive drives the car one time. If there is not enough battery to drive one more time,
-// // the car will not move.
-// func Drive(car Car) Car {
-// 	panic("Please implement the Drive function")
-// }
-
-// // CanFinish checks if a car is able to finish a certain track.
-// func CanFinish(car Car, track Track) bool {
-// 	panic("Please implement the CanFinish function")
-// }
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	if car.battery/car.batteryDrain >= 1 {
+		return true
+	}
+	return false
+}
