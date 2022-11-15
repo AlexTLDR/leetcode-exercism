8f189319ba972b26a1aabcb3c47d658acfd9634b
Alex <abadragan@gmail.com>
2022-11-15 11:14:28 +0100

    created NewCar

--- exercism/Need For Speed/main.go
+++ exercism/Need For Speed/main.go
@@ -2,28 +2,40 @@
 
 package main
 
-func main() {
+import "fmt"
 
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
+
+func main() {
+	speed := 5
+	batteryDrain := 2
+	car := NewCar(speed, batteryDrain)
+	fmt.Println(car)
 }
 
 func NewCar(speed, batteryDrain int) Car {
-	panic("Please implement the NewCar function")
+	return Car{speed: speed, batteryDrain: batteryDrain}
 }
 
 // TODO: define the 'Track' type struct
 
 // NewTrack creates a new track
-func NewTrack(distance int) Track {
-	panic("Please implement the NewTrack function")
-}
+// func NewTrack(distance int) Track {
+// 	panic("Please implement the NewTrack function")
+// }
 
-// Drive drives the car one time. If there is not enough battery to drive one more time,
-// the car will not move.
-func Drive(car Car) Car {
-	panic("Please implement the Drive function")
-}
+// // Drive drives the car one time. If there is not enough battery to drive one more time,
+// // the car will not move.
+// func Drive(car Car) Car {
+// 	panic("Please implement the Drive function")
+// }
 
-// CanFinish checks if a car is able to finish a certain track.
-func CanFinish(car Car, track Track) bool {
-	panic("Please implement the CanFinish function")
-}
+// // CanFinish checks if a car is able to finish a certain track.
+// func CanFinish(car Car, track Track) bool {
+// 	panic("Please implement the CanFinish function")
+// }
