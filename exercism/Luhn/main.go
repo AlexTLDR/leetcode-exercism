dc2c58921246df195bafec716c8e95c8d0adebcb
Alex <abadragan@gmail.com>
2022-11-15 15:08:45 +0100

    refactored to use only a single loop

--- exercism/Luhn/main.go
+++ exercism/Luhn/main.go
@@ -14,49 +14,23 @@ func main() {
 
 func Valid(id string) bool {
 	trimmed := strings.Replace(id, " ", "", -1)
-
+	var sum int
 	if len(trimmed) < 2 {
 		return false
 	}
-	for _, v := range trimmed {
+	for i, v := range trimmed {
 		if !unicode.IsDigit(v) {
 			return false
 		}
+		digit := int(v - '0')
+		switch {
+		case i%2 == len(trimmed)%2:
+			sum += doubleDigitValue(digit)
+		default:
+			sum += digit
+		}
 	}
-	var sum int
-	var idArr []int
-
-	for _, v := range trimmed {
-		idArr = append(idArr, int(v-48))
-	}
-	switch len(idArr)%2 == 0 {
-	case true:
-
-		for i, v := range idArr {
-			if i == 0 || i%2 == 0 {
-				v = doubleDigitValue(v)
-			}
-			sum += v
-		}
-		if sum%10 == 0 {
-			return true
-		}
-		return false
-
-	case false:
-		for i, v := range idArr {
-			if i == 1 || i%2 != 0 {
-				v = doubleDigitValue(v)
-			}
-			sum += v
-		}
-		if sum%10 == 0 {
-			return true
-		}
-
-	}
-
-	return false
+	return sum%10 == 0
 }
 
 func doubleDigitValue(digit int) (value int) {
