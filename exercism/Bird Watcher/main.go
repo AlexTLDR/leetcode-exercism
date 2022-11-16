fa817891ac2f4fb1e9e76f9a8fe533b244b30e76
Alex <abadragan@gmail.com>
2022-11-16 09:48:46 +0100

    7 tests passed, 3 failed

--- exercism/Bird Watcher/main.go
+++ exercism/Bird Watcher/main.go
@@ -2,24 +2,43 @@
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	birdsPerDay := []int{2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1}
+	fmt.Println(TotalBirdCount(birdsPerDay))
+	fmt.Println(BirdsInWeek(birdsPerDay, 2))
+	fmt.Println(FixBirdCountLog(birdsPerDay))
 }
 
 // TotalBirdCount return the total bird count by summing
 // the individual day's counts.
 func TotalBirdCount(birdsPerDay []int) int {
-	panic("Please implement the TotalBirdCount() function")
+	total := 0
+	for _, v := range birdsPerDay {
+		total += v
+	}
+	return total
 }
 
 // BirdsInWeek returns the total bird count by summing
 // only the items belonging to the given week.
 func BirdsInWeek(birdsPerDay []int, week int) int {
-	panic("Please implement the BirdsInWeek() function")
+	total := 0
+
+	for i := 7; i < len(birdsPerDay); i++ {
+		total += birdsPerDay[i]
+	}
+	return total
 }
 
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
 func FixBirdCountLog(birdsPerDay []int) []int {
-	panic("Please implement the FixBirdCountLog() function")
+	for i := 0; i < len(birdsPerDay); i++ {
+		if i == 0 || i%2 == 0 {
+			birdsPerDay[i] = birdsPerDay[i] + 1
+		}
+	}
+	return birdsPerDay
 }
