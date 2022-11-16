f6a8b0c70137d58788b12fad022d8fdfc7b3cd57
Alex <abadragan@gmail.com>
2022-11-16 12:54:28 +0100

    10 tests passed, 2 tests failed

--- exercism/Lasagna Master/main.go
+++ exercism/Lasagna Master/main.go
@@ -6,9 +6,16 @@ import "fmt"
 
 func main() {
 	layers := []string{"sauce", "noodles", "sauce", "meat", "mozzarella", "noodles"}
-	quantities := []string{"sauce", "noodles", "sauce", "meat", "mozzarella", "noodles"}
+	quantitiesStrings := []string{"sauce", "noodles", "sauce", "meat", "mozzarella", "noodles"}
+	friendsList := []string{"noodles", "sauce", "mozzarella", "kampot pepper"}
+	myList := []string{"noodles", "meat", "sauce", "mozzarella", "?"}
+	quantitiesFloats := []float64{1.2, 3.6, 10.5}
+	scaledQuantities := ScaleRecipe(quantitiesFloats, 4)
 	fmt.Println(PreparationTime(layers, 0))
-	fmt.Println(Quantities(quantities))
+	fmt.Println(Quantities(quantitiesStrings))
+	AddSecretIngredient(friendsList, myList)
+	fmt.Println(myList)
+	fmt.Println(scaledQuantities)
 }
 
 func PreparationTime(layers []string, preparation int) int {
@@ -28,4 +35,15 @@ func Quantities(layers []string) (noodles int, sauce float64) {
 		}
 	}
 	return noodles, sauce
+}
+
+func AddSecretIngredient(friendsList, myList []string) {
+	myList[len(myList)-1] = friendsList[len(friendsList)-1]
+}
+
+func ScaleRecipe(quantities []float64, portions int) (scaledQuantities []float64) {
+	for _, v := range quantities {
+		scaledQuantities = append(scaledQuantities, v*float64(portions/2))
+	}
+	return
 }
