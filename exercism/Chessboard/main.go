7a380bf175b97a57775a0df3ebbecf4ac6a6e2a6
Alex <abadragan@gmail.com>
2022-12-14 13:56:17 +0100

    implemented the CountInFile

--- exercism/Chessboard/main.go
+++ exercism/Chessboard/main.go
@@ -12,9 +12,42 @@ type Chessboard map[string]File
 
 func main() {
 	var A File = []bool{true, false, true, false, false, false, false, true}
+	var B File = []bool{false, false, false, false, true, false, false, false}
+	var C File = []bool{false, false, true, false, false, false, false, false}
+	var D File = []bool{false, false, false, false, false, false, false, false}
+	var E File = []bool{false, false, false, false, false, true, false, true}
+	var F File = []bool{false, false, false, false, false, false, false, false}
+	var G File = []bool{false, false, false, true, false, false, false, false}
+	var H File = []bool{true, true, true, true, true, true, false, true}
+	board := map[string]File{
+		"A": A,
+		"B": B,
+		"C": C,
+		"D": D,
+		"E": E,
+		"F": F,
+		"G": G,
+		"H": H,
+	}
 	//A = []bool{true}
-	fmt.Printf("%v, %T\n", A, A)
+	//fmt.Printf("%v, %T\n", A, A)
+	//cb := newChessboard()
+	fmt.Println(CountInFile(board, "A"))
+	//fmt.Println(CountInRank(board))
 
+}
+
+func newChessboard() Chessboard {
+	return Chessboard{
+		"A": File{true, false, true, false, false, false, false, true},
+		"B": File{false, false, false, false, true, false, false, false},
+		"C": File{false, false, true, false, false, false, false, false},
+		"D": File{false, false, false, false, false, false, false, false},
+		"E": File{false, false, false, false, false, true, false, true},
+		"F": File{false, false, false, false, false, false, false, false},
+		"G": File{false, false, false, true, false, false, false, false},
+		"H": File{true, true, true, true, true, true, false, true},
+	}
 }
 
 // CountInFile returns how many squares are occupied in the chessboard,
