7d0a48ec75b9cd74b77145de0d2230b74de8cc0a
AlexFZR <abadragan@gmail.com>
2022-12-14 11:44:23 +0100

    declaring the File(s)

--- exercism/Chessboard/main.go
+++ exercism/Chessboard/main.go
@@ -1,17 +1,32 @@
 package main
 
-func main() {
-
-}
+import "fmt"
 
 // Declare a type named File which stores if a square is occupied by a piece - this will be a slice of bools
 
+type File []bool
+
 // Declare a type named Chessboard which contains a map of eight Files, accessed with keys from "A" to "H"
+
+type Chessboard map[string]File
+
+func main() {
+	var A File = []bool{true, false, true, false, false, false, false, true}
+	//A = []bool{true}
+	fmt.Printf("%v, %T\n", A, A)
+
+}
 
 // CountInFile returns how many squares are occupied in the chessboard,
 // within the given file.
 func CountInFile(cb Chessboard, file string) int {
-	panic("Please implement CountInFile()")
+	count := 0
+	for _, v := range cb[file] {
+		if v {
+			count++
+		}
+	}
+	return count
 }
 
 // CountInRank returns how many squares are occupied in the chessboard,
