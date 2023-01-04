ff3236f8186770aa592a92ae18d0c49e6cffa84a
AlexFZR <abadragan@gmail.com>
2023-01-04 10:40:51 +0100

    passed all the tests

--- exercism/Clock/main.go
+++ exercism/Clock/main.go
@@ -4,19 +4,12 @@ package main
 
 import "fmt"
 
-const minsInDay = 24 * 60
-
 type Clock struct {
 	hours   int
 	minutes int
 }
 
 func main() {
-	// fmt.Println(New(144, 60))
-	//c := Clock{23, 15}
-
-	// fmt.Println(c.String())
-	//fmt.Println(New(0, 160))
 
 	c := New(0, 10)
 	fmt.Println(c)
@@ -40,34 +33,13 @@ func New(h, m int) Clock {
 }
 
 func (c Clock) Add(m int) Clock {
-	c.minutes += m
-	if c.minutes >= 60 {
-		c.hours += c.minutes / 60
-		c.minutes %= 60
-	}
-	if c.hours >= 24 {
-		c.hours %= 24
-	}
-	return c
+
+	return New(c.hours, c.minutes+m)
 }
 
 func (c Clock) Subtract(m int) Clock {
-	// c.minutes -= m
-	// if c.minutes < 0 {
-	// 	fmt.Println("from subtract", c.hours, c.minutes)
-	// 	c.hours -= 1 + c.minutes/60
-	// 	fmt.Println(c.hours)
-	// 	c.minutes = 60 + c.minutes%60
-	// 	if c.minutes == 60 {
-	// 		c.minutes = 0
-	// 		c.hours += 1
-	// 	}
-	// }
-	// if c.hours >= 24 {
-	// 	c.hours %= 24
-	// }
-	return c.Add(-m)
 
+	return New(c.hours, c.minutes-m)
 }
 
 func (c Clock) String() string {
