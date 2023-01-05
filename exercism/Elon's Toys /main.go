e6251bf6ad98a34aada2664d25530132c16fbc72
AlexFZR <abadragan@gmail.com>
2023-01-05 14:54:03 +0100

    passed all the tests

--- exercism/Elon's Toys /main.go
+++ exercism/Elon's Toys /main.go
@@ -76,6 +76,18 @@ func (car *Car) Drive() {
 
 // TODO: define the 'DisplayDistance() string' method
 
+func (car *Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", car.distance)
+}
+
 // TODO: define the 'DisplayBattery() string' method
 
+func (car *Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", car.battery)
+}
+
 // TODO: define the 'CanFinish(trackDistance int) bool' method
+
+func (car *Car) CanFinish(trackDistance int) bool {
+	return trackDistance <= car.speed*(car.battery/car.batteryDrain)
+}
