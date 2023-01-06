fc759ff6dd4249ac4925405d49f420fae8afe6b7
AlexFZR <abadragan@gmail.com>
2023-01-06 10:50:53 +0100

    Tournament initial commit

---
+++ exercism/Tournament /main.go
@@ -0,0 +1,13 @@
+// https://exercism.org/tracks/go/exercises/tournament
+
+package main
+
+import "io"
+
+func main() {
+
+}
+
+func Tally(reader io.Reader, writer io.Writer) error {
+	panic("Please implement the Tally function")
+}
