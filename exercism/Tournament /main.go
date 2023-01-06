58c25d7991f96a77c9622c3fc47a5f0062bddda4
AlexFZR <abadragan@gmail.com>
2023-01-06 15:07:25 +0100

    correctly adding the points to the teams

--- exercism/Tournament /main.go
+++ exercism/Tournament /main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 const (
@@ -24,14 +25,43 @@ func ReadFile() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
-	for fileScanner.Scan() {
-		fmt.Println(fileScanner.Text())
-	}
-	readFile.Close()
+
+	defer readFile.Close()
+	Tally(readFile, nil)
+
 }
 
 func Tally(reader io.Reader, writer io.Writer) error {
-	panic("Please implement the Tally function")
+
+	teams := map[string]int{
+		"Allegoric Alaskans":      0,
+		"Blithering Badgers":      0,
+		"Devastating Donkeys":     0,
+		"Courageous Californians": 0,
+	}
+	fileScanner := bufio.NewScanner(reader)
+	fileScanner.Split(bufio.ScanLines)
+	fileLines := []string{}
+	for fileScanner.Scan() {
+		fileLines = append(fileLines, fileScanner.Text())
+	}
+
+	for _, line := range fileLines {
+
+		slice := strings.Split(line, ";")
+		//fmt.Println("line number: ", slice)
+		switch slice[2] {
+		case "win":
+			//fmt.Println(slice[0])
+			teams[slice[0]] += 3
+		case "draw":
+			teams[slice[0]] += 1
+			teams[slice[1]] += 1
+		case "loss":
+			teams[slice[1]] += 3
+		}
+	}
+	fmt.Println(teams)
+	fmt.Println(fileLines)
+	return nil
 }
