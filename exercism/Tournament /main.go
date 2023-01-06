e1f430bac4eab2cb42e22156d94e6ca1b468e018
AlexFZR <abadragan@gmail.com>
2023-01-06 11:19:51 +0100

    implemented the ReadFile func

--- exercism/Tournament /main.go
+++ exercism/Tournament /main.go
@@ -2,10 +2,34 @@
 
 package main
 
-import "io"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	win  = 3
+	draw = 1
+	loss = 0
+)
 
 func main() {
+	ReadFile()
+}
 
+func ReadFile() {
+	readFile, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+	readFile.Close()
 }
 
 func Tally(reader io.Reader, writer io.Writer) error {
