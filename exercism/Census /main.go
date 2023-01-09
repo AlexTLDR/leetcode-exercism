73e9163bfff2dee2907ce67fe7fab3933fcc0789
Alex <abadragan@gmail.com>
2023-01-09 09:43:19 +0100

    implemented the NewResident func

--- exercism/Census /main.go
+++ exercism/Census /main.go
@@ -2,7 +2,10 @@
 
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println(NewResident("Matthew Sanabria", 29, map[string]string{"street": "Main St."}))
 
 }
 
@@ -15,7 +18,11 @@ type Resident struct {
 
 // NewResident registers a new resident in this city.
 func NewResident(name string, age int, address map[string]string) *Resident {
-	panic("Please implement NewResident.")
+	return &Resident{
+		Name:    name,
+		Age:     age,
+		Address: address,
+	}
 }
 
 // HasRequiredInfo determines if a given resident has all of the required information.
