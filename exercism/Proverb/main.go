23e6ab3a9af44733f8a28171afdcc6bc6bd26048
Alex <abadragan@gmail.com>
2023-01-10 09:47:15 +0100

    Proverb initial commit

---
+++ exercism/Proverb/main.go
@@ -0,0 +1,15 @@
+// https://exercism.org/tracks/go/exercises/proverb
+
+package main
+
+import "fmt"
+
+func main() {
+	rhyme := []string{"nail", "shoe", "horse", "rider", "message", "battle", "kingdom"}
+	fmt.Println(Proverb(rhyme))
+}
+
+func Proverb(rhyme []string) []string {
+
+	panic("Please implement the Proverb function")
+}
