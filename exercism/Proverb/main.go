79702674b98fefa6bfcff054b72cda7bf67eed35
Alex <abadragan@gmail.com>
2023-01-10 10:07:20 +0100

    tested with print statements

--- exercism/Proverb/main.go
+++ exercism/Proverb/main.go
@@ -10,6 +10,13 @@ func main() {
 }
 
 func Proverb(rhyme []string) []string {
+	if len(rhyme) == 0 {
+		return []string{}
+	}
 
-	panic("Please implement the Proverb function")
+	for i := 0; i < len(rhyme)-1; i++ {
+		fmt.Printf("For want of a %s the %s was lost.\n", rhyme[i], rhyme[i+1])
+	}
+	fmt.Printf("And all for the want of a %s.\n", rhyme[0])
+	return []string{}
 }
