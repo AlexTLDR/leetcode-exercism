57ec369e0759a4aea6657855fa882491dff8dc14
Alex <abadragan@gmail.com>
2023-01-11 13:27:07 +0100

    passed all the tests

--- exercism/Reverse String /main.go
+++ exercism/Reverse String /main.go
@@ -13,5 +13,9 @@ func main() {
 }
 
 func Reverse(input string) string {
-	return input
+	inputArr := []rune(input)
+	for i, j := 0, len(inputArr)-1; i < j; i, j = i+1, j-1 {
+		inputArr[i], inputArr[j] = inputArr[j], inputArr[i]
+	}
+	return string(inputArr)
 }
