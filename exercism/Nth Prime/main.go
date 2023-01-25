53b54ba4764bd1b761b5d1094c33d67d17e48285
Alex <abadragan@gmail.com>
2023-01-25 21:38:30 +0100

    testing the prime number validation

--- exercism/Nth Prime/main.go
+++ exercism/Nth Prime/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println(Nth(6))
 }
 
 func Nth(n int) (int, error) {
-	panic("Please implement the Nth function")
+	sqRoot := int(math.Sqrt(float64(n)))
+	for i := 2; i <= sqRoot; i++ {
+		if n%i == 0 {
+			fmt.Println("Non Prime Number")
+			return 0, fmt.Errorf("Non Prime Number")
+		}
+	}
+	return n, fmt.Errorf("")
 }
