5bee7205595c2c43bc6e8019af9c4a5ffc95e66e
AlexFZR <abadragan@gmail.com>
2023-03-13 20:14:42 +0100

    Resistor Color initial commit

---
+++ exercism/Resistor Color /main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+}
+
+// Colors should return the list of all colors.
+func Colors() []string {
+	panic("Please implement the Colors function")
+}
+
+// ColorCode returns the resistance value of the given color.
+func ColorCode(color string) int {
+	panic("Please implement the ColorCode function")
+}
