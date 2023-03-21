e723cdce7f7b239523984eee955f510805335c6d
Alex <abadragan@gmail.com>
2023-03-21 11:02:57 +0100

    Resistor Color Trio initial commit

---
+++ exercism/Resistor Color Trio /main.go
@@ -0,0 +1,18 @@
+//https://exercism.org/tracks/go/exercises/resistor-color-trio/edit
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(Label([]string{"orange", "orange", "black"}))
+
+}
+
+// Label describes the resistance value given the colors of a resistor.
+// The label is a string with a resistance value with an unit appended
+// (e.g. "33 ohms", "470 kiloohms").
+func Label(colors []string) string {
+	return "To Do"
+}
