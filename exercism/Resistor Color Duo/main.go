c0c7c062f3b04fbccf54483b356e48623d5905e5
Alex <abadragan@gmail.com>
2023-03-21 10:51:01 +0100

    switched to slice

--- exercism/Resistor Color Duo/main.go
+++ exercism/Resistor Color Duo/main.go
@@ -5,33 +5,25 @@ import (
 	"strconv"
 )
 
-var codes = map[string]int{
-	"black":  0,
-	"brown":  1,
-	"red":    2,
-	"orange": 3,
-	"yellow": 4,
-	"green":  5,
-	"blue":   6,
-	"violet": 7,
-	"grey":   8,
-	"white":  9,
-}
+var codes = []string{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
 
 func main() {
-	fmt.Println(Value([]string{"brown", "green"}))
+	fmt.Println(Value([]string{"brown", "green", "violet"}))
 }
 
 // Value should return the resistance value of a resistor with a given colors.
 func Value(colors []string) int {
 	var resistance string
-	for _, color := range colors {
-		for code, value := range codes {
-			if color == code {
-				resistance = resistance + strconv.Itoa(value)
-			}
-		}
+	for i := 0; i < 2; i++ {
+		fmt.Println(codes[i])
 	}
+	// for _, color := range colors {
+	// 	for code, value := range codes {
+	// 		if color == code {
+	// 			resistance = resistance + strconv.Itoa(value)
+	// 		}
+	// 	}
+	// }
 	result, _ := strconv.Atoi(resistance)
 	return result
 }
