d1a277e5dc7d0f6edb6c421bf375ccdcd8ad3d9a
Alex <abadragan@gmail.com>
2023-03-21 10:25:03 +0100

    created the colors map

--- exercism/Resistor Color Duo/main.go
+++ exercism/Resistor Color Duo/main.go
@@ -1,7 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+var colors = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
+func main() {
+	fmt.Println(Value([]string{"brown", "green"}))
 }
 
 // Value should return the resistance value of a resistor with a given colors.
