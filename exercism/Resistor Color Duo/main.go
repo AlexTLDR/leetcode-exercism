6ca829684b3943d677e1174465bd1b4ca327cbfe
Alex <abadragan@gmail.com>
2023-03-21 10:16:07 +0100

    Resistor Color Duo initial commit

---
+++ exercism/Resistor Color Duo/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+
+}
+
+// Value should return the resistance value of a resistor with a given colors.
+func Value(colors []string) int {
+	panic("Implement the Value function")
+}
