68c81e873fa74ec3e7e53683200112c42accc53d
Alex <abadragan@gmail.com>
2023-03-21 10:42:00 +0100

    implemented the return logic

--- exercism/Resistor Color Duo/main.go
+++ exercism/Resistor Color Duo/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-var colors = map[string]int{
+var codes = map[string]int{
 	"black":  0,
 	"brown":  1,
 	"red":    2,
@@ -21,5 +24,14 @@ func main() {
 
 // Value should return the resistance value of a resistor with a given colors.
 func Value(colors []string) int {
-	panic("Implement the Value function")
+	var resistance string
+	for _, color := range colors {
+		for code, value := range codes {
+			if color == code {
+				resistance = resistance + strconv.Itoa(value)
+			}
+		}
+	}
+	result, _ := strconv.Atoi(resistance)
+	return result
 }
