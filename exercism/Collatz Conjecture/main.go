916a44b5d22f51ff2c25fa235013ad1ea40298a5
Alex <abadragan@gmail.com>
2023-03-22 11:50:12 +0100

    implemented the non-recursive solution

--- exercism/Collatz Conjecture/main.go
+++ exercism/Collatz Conjecture/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 var count = 0
 
 func main() {
-	fmt.Println(CollatzConjecture(1000000))
+	fmt.Println(CollatzConjecture(12))
+	fmt.Println(CollatzConjecture2(12))
 }
 
+// since exercism is failing my recursive function I created CollatzConjecture2 without recursion to pass the exercism tests
 func CollatzConjecture(n int) (int, error) {
 	switch {
 	case n < 1:
-		return count, fmt.Errorf("n must be greater or equal to 1")
+		return count, errors.New("n must be greater or equal to 1")
 	case n == 1:
 		return count, nil
 	case n%2 == 0:
@@ -24,4 +27,21 @@ func CollatzConjecture(n int) (int, error) {
 	count++
 	CollatzConjecture(n)
 	return count, nil
+}
+
+func CollatzConjecture2(n int) (int, error) {
+	steps := 0
+	if n < 1 {
+		return steps, errors.New("n must be greater or equal to 1")
+	}
+
+	for n != 1 {
+		steps++
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+	}
+	return steps, nil
 }
