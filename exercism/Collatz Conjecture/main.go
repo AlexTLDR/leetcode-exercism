514c572a1e8cfcc466bef01b761287c5fde77375
Alex <abadragan@gmail.com>
2023-03-22 10:14:01 +0100

    Collatz Conjecture initial commit

---
+++ exercism/Collatz Conjecture/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(CollatzConjecture(12))
+}
+
+func CollatzConjecture(n int) (int, error) {
+	panic("Please implement the CollatzConjecture function")
+}
