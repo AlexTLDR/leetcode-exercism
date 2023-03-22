3d7e28ceb2b8259818c4610aa110e92337442cf4
Alex <abadragan@gmail.com>
2023-03-22 10:58:38 +0100

    implemented the counter

--- exercism/Collatz Conjecture/main.go
+++ exercism/Collatz Conjecture/main.go
@@ -2,23 +2,22 @@ package main
 
 import "fmt"
 
+var count = 0
+
 func main() {
 	fmt.Println(CollatzConjecture(12))
 }
 
 func CollatzConjecture(n int) (int, error) {
-	steps := 0
-	fmt.Println(n)
 	switch {
 	case n == 1:
-		return steps, nil
+		return count, nil
 	case n%2 == 0:
-		steps++
 		n /= 2
 	default:
-		steps++
 		n = n*3 + 1
 	}
+	count++
 	CollatzConjecture(n)
-	return steps, nil
+	return count, nil
 }
