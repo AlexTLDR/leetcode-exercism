cd82870f8d04f05311d7ced3f0f170952db8321c
Alex <abadragan@gmail.com>
2023-03-23 09:48:41 +0100

    passed all the tests

--- exercism/Gigasecond /main.go
+++ exercism/Gigasecond /main.go
@@ -9,9 +9,6 @@ func main() {
 }
 
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return t
+	gigaSecond := 1000000000 * time.Second
+	return t.Add(gigaSecond)
 }
