004566abd84ab738d4212defd712b84525af3fe8
Alex <abadragan@gmail.com>
2023-03-23 10:31:47 +0100

    7 passed 6 failed

--- exercism/Darts /main.go
+++ exercism/Darts /main.go
@@ -2,10 +2,30 @@
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(Score(1, 1))
 }
 
 func Score(x, y float64) int {
-	panic("Please implement the Score function")
+	score := 0.0
+	switch {
+	case x >= y:
+		score = x
+	default:
+		score = y
+	}
+
+	switch {
+	case score > 10:
+		score = 0
+	case score > 5 && score <= 10:
+		score = 1
+	case score > 1 && score <= 5:
+		score = 5
+	default:
+		score = 10
+	}
+	return int(score)
 }
