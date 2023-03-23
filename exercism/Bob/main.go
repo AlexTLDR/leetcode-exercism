381f86edb46e062fe318eddf53ea208291c4a2ed
Alex <abadragan@gmail.com>
2023-03-23 11:02:40 +0100

    Bob initial commit

---
+++ exercism/Bob/main.go
@@ -0,0 +1,14 @@
+// https://exercism.org/tracks/go/exercises/bob
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Hey("HEY BOB!"))
+}
+
+func Hey(remark string) string {
+
+	return ""
+}
