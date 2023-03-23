2c5f1ecaec535e60a9344977330f275f51e4b246
Alex <abadragan@gmail.com>
2023-03-23 09:09:58 +0100

    Gigasecond initial commit

---
+++ exercism/Gigasecond /main.go
@@ -0,0 +1,17 @@
+//https://exercism.org/tracks/go/exercises/gigasecond
+
+package main
+
+import "time"
+
+func main() {
+
+}
+
+func AddGigasecond(t time.Time) time.Time {
+	// Write some code here to pass the test suite.
+	// Then remove all the stock comments.
+	// They're here to help you get started but they only clutter a finished solution.
+	// If you leave them in, reviewers may protest!
+	return t
+}
