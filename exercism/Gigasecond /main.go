8e02a16d7dad85b92d0c36fd58079a782df7f8a7
Alex <abadragan@gmail.com>
2023-03-23 09:54:57 +0100

    added the AddGigasecond func call

--- exercism/Gigasecond /main.go
+++ exercism/Gigasecond /main.go
@@ -2,13 +2,17 @@
 
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-
+	fmt.Println(AddGigasecond(time.Date(2046, time.October, 2, 23, 46, 40, 0, time.Local)))
 }
 
 func AddGigasecond(t time.Time) time.Time {
-	gigaSecond := 1000000000 * time.Second
+	//gigaSecond := 1000000000 * time.Second
+	gigaSecond := 1e9 * time.Second
 	return t.Add(gigaSecond)
 }
