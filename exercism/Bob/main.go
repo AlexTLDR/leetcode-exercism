7534452c75809f77effefe314dc440bcd76d1ef9
Alex <abadragan@gmail.com>
2023-03-23 12:02:41 +0100

    18 passed 7 failed

--- exercism/Bob/main.go
+++ exercism/Bob/main.go
@@ -5,25 +5,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Hey(""))
+	fmt.Println(Hey("WATCH OUT!"))
 }
 
 func Hey(remark string) string {
 
 	for _, r := range remark {
-		if Capitals(r) == "c" && remark[len(remark)-1] == 63 {
+		if Capitals(r) != "lc" && remark[len(remark)-1] == 63 {
 			return "Sure."
+		} else if Capitals(r) == "lc" {
+			return "Whatever."
 		}
 		if r == 0 {
 			return "Fine. Be that way!"
 		}
 	}
 	for _, r := range remark {
-		if Capitals(r) == "lc" && remark[len(remark)-1] == 63 {
+		if Capitals(r) == "c" && remark[len(remark)-1] == 63 {
 			return "Calm down, I know what I'm doing!"
-		} else {
+		} else if Capitals(r) == "c" {
 			return "Whoa, chill out!"
 		}
+
 	}
 	return "Whatever."
 }
@@ -31,9 +34,9 @@ func Hey(remark string) string {
 func Capitals(i rune) string {
 	switch {
 	case i >= 97 && i <= 122:
-		return "c"
-	case i >= 65 && i <= 90:
 		return "lc"
+	case i >= 65 && i <= 90:
+		return "c"
 	}
 	return "na"
 }
