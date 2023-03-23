59f51d9ee4a87f535b384da6590481ca797d0776
Alex <abadragan@gmail.com>
2023-03-23 11:34:10 +0100

    trying to implement a func for capital letters

--- exercism/Bob/main.go
+++ exercism/Bob/main.go
@@ -5,10 +5,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Hey("HEY BOB!"))
+	fmt.Println(Hey("hEy BOB?"))
 }
 
 func Hey(remark string) string {
 
-	return ""
+	for _, r := range remark {
+		if Capitals(r) == "c" && remark[len(remark)-1] == 63 {
+			return "Sure."
+		}
+		if Capitals(r) == "lc" && remark[len(remark)-1] == 63 {
+			return "Whoa, chill out!"
+		}
+	}
+	return "Whatever."
+}
+
+func Capitals(i rune) string {
+	switch {
+	case i >= 97 && i <= 122:
+		return "c"
+	case i >= 65 && i <= 90:
+		return "lc"
+	}
+	return "na"
 }
