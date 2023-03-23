1058ad7769fb414e50dc98d98b180cb87f0a0c50
Alex <abadragan@gmail.com>
2023-03-23 10:55:11 +0100

    passed all the tests

--- exercism/Darts /main.go
+++ exercism/Darts /main.go
@@ -12,24 +12,15 @@ func main() {
 }
 
 func Score(x, y float64) int {
-	score := 0.0
-	x, y = math.Abs(x), math.Abs(y)
+	score := math.Pow(x, 2) + math.Pow(y, 2)
 	switch {
-	case x >= y:
-		score = x
-	default:
-		score = y
+	case score > 100:
+		return 0
+	case score > 25:
+		return 1
+	case score > 1:
+		return 5
 	}
 
-	switch {
-	case score > 10:
-		score = 0
-	case score > 5 && score <= 10:
-		score = 1
-	case score > 1 && score <= 5:
-		score = 5
-	default:
-		score = 10
-	}
-	return int(score)
+	return 10
 }
