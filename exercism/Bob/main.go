e6c3a7326b577233e05aab5951abd418869d0e45
Alex <abadragan@gmail.com>
2023-03-24 10:47:14 +0100

    failing non letter tests

--- exercism/Bob/main.go
+++ exercism/Bob/main.go
@@ -9,36 +9,14 @@ import (
 )
 
 func main() {
-	fmt.Println(Hey("TBBBBBBAAA?"))
+	fmt.Println(Hey(""))
 }
 
 func Hey(remark string) string {
-	// if len(remark) == 0 {
-	// 	return "Fine. Be that way!"
-	// }
-	// for _, r := range remark {
-	// 	if r == 32 {
-	// 		return "Fine. Be that way!"
-	// 	}
-	// 	if unicode.IsLower(r) && remark[len(remark)-1] == 63 {
-	// 		return "Sure."
-	// 	}
-	// }
+	if len(remark) == 0 {
+		return "Fine. Be that way!"
+	}
 
-	// for _, r := range remark {
-	// 	if unicode.IsLower(r) {
-	// 		return "Whatever."
-	// 	}
-	// }
-	// for _, r := range remark {
-	// 	if unicode.IsUpper(r) && remark[len(remark)-1] == 63 {
-	// 		return "Calm down, I know what I'm doing!"
-	// 	} else if unicode.IsUpper(r) {
-	// 		return "Whoa, chill out!"
-	// 	}
-
-	// }
-	// return "Whatever."
 	remark = strings.Replace(remark, " ", "", -1)
 	remark = strings.Replace(remark, "\t", "", -1)
 	remark = strings.Replace(remark, "\n", "", -1)
@@ -58,5 +36,13 @@ func Hey(remark string) string {
 		}
 	}
 
-	return ""
+	if !hasLower {
+		return "Whoa, chill out!"
+	}
+	for _, r := range remark {
+		if r == 32 {
+			return "Fine. Be that way!"
+		}
+	}
+	return "Whatever."
 }
