91371dfa320518b954478b9864dc69edea08fe24
Alex <abadragan@gmail.com>
2023-03-24 10:34:14 +0100

    handled the question mark

--- exercism/Bob/main.go
+++ exercism/Bob/main.go
@@ -4,38 +4,59 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"unicode"
 )
 
 func main() {
-	fmt.Println(Hey("fffbbcbeab?"))
+	fmt.Println(Hey("TBBBBBBAAA?"))
 }
 
 func Hey(remark string) string {
-	if len(remark) == 0 {
-		return "Fine. Be that way!"
-	}
-	for _, r := range remark {
-		if r == 32 {
-			return "Fine. Be that way!"
-		}
-		if unicode.IsLower(r) && remark[len(remark)-1] == 63 {
-			return "Sure."
-		}
-	}
+	// if len(remark) == 0 {
+	// 	return "Fine. Be that way!"
+	// }
+	// for _, r := range remark {
+	// 	if r == 32 {
+	// 		return "Fine. Be that way!"
+	// 	}
+	// 	if unicode.IsLower(r) && remark[len(remark)-1] == 63 {
+	// 		return "Sure."
+	// 	}
+	// }
 
+	// for _, r := range remark {
+	// 	if unicode.IsLower(r) {
+	// 		return "Whatever."
+	// 	}
+	// }
+	// for _, r := range remark {
+	// 	if unicode.IsUpper(r) && remark[len(remark)-1] == 63 {
+	// 		return "Calm down, I know what I'm doing!"
+	// 	} else if unicode.IsUpper(r) {
+	// 		return "Whoa, chill out!"
+	// 	}
+
+	// }
+	// return "Whatever."
+	remark = strings.Replace(remark, " ", "", -1)
+	remark = strings.Replace(remark, "\t", "", -1)
+	remark = strings.Replace(remark, "\n", "", -1)
+	remark = strings.Replace(remark, "\r", "", -1)
+	// hasUpper := false
+	hasLower := false
 	for _, r := range remark {
 		if unicode.IsLower(r) {
-			return "Whatever."
+			hasLower = true
 		}
 	}
-	for _, r := range remark {
-		if unicode.IsUpper(r) && remark[len(remark)-1] == 63 {
+	if remark[len(remark)-1] == 63 {
+		if hasLower {
+			return "Sure."
+		} else {
 			return "Calm down, I know what I'm doing!"
-		} else if unicode.IsUpper(r) {
-			return "Whoa, chill out!"
 		}
-
 	}
-	return "Whatever."
+
+	return ""
 }
