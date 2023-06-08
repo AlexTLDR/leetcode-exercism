a8231acb28de3cc1375885bbff7e02f47b92898f
AlexFZR <abadragan@gmail.com>
2023-06-08 18:22:17 +0200

    word count initial commit

---
+++ exercism/Word Count /main.go
@@ -0,0 +1,13 @@
+// https://exercism.org/tracks/go/exercises/word-count
+
+package main
+
+func main() {
+
+}
+
+type Frequency map[string]int
+
+func WordCount(phrase string) Frequency {
+	panic("Please implement the WordCount function")
+}
