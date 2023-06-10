2dada0814183573df2b378ed57c8f8953f076795
AlexFZR <abadragan@gmail.com>
2023-06-10 13:47:24 +0200

    validated ISBN calculator

--- exercism/ISBN Verifier /main.go
+++ exercism/ISBN Verifier /main.go
@@ -1,11 +1,27 @@
 // https://exercism.org/tracks/go/exercises/isbn-verifier
 
+// ISBN formula => (d₁ * 10 + d₂ * 9 + d₃ * 8 + d₄ * 7 + d₅ * 6 + d₆ * 5 + d₇ * 4 + d₈ * 3 + d₉ * 2 + d₁₀ * 1) mod 11 == 0
+
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	fmt.Println(IsValidISBN("3-598-21508-8"))
 }
 
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	trimmed := strings.Replace(isbn, "-", "", -1)
+
+	var ISBN int
+	for i := 0; i < len(trimmed); i++ {
+		ISBN += (int(trimmed[0]) - 48) * (len(trimmed) - i)
+	}
+	if ISBN%11 == 0 {
+		return true
+	}
+	return false
 }
