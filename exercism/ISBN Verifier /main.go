9837733ca04f7dc4e011bf52de672bbd532ff87f
AlexFZR <abadragan@gmail.com>
2023-06-10 13:17:34 +0200

    ISBN Verifier initial commit

---
+++ exercism/ISBN Verifier /main.go
@@ -0,0 +1,11 @@
+// https://exercism.org/tracks/go/exercises/isbn-verifier
+
+package main
+
+func main() {
+
+}
+
+func IsValidISBN(isbn string) bool {
+	panic("Please implement the IsValidISBN function")
+}
