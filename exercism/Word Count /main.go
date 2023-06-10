ec6c7ab430eecedebfc7bddc170b19e5c9c56376
AlexFZR <abadragan@gmail.com>
2023-06-10 12:42:54 +0200

    10 passed, 3 failed

--- exercism/Word Count /main.go
+++ exercism/Word Count /main.go
@@ -16,7 +16,7 @@ type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
 	wordCount := make(map[string]int)
-	words := regexp.MustCompile(`[a-zA-Z]+`).FindAllString(phrase, -1)
+	words := regexp.MustCompile(`[a-z0-9'A-Z]+`).FindAllString(phrase, -1)
 
 	for _, w := range words {
 		wordCount[strings.ToLower(w)]++
