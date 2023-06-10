98723eaa526c31db1fccd5118dcb06405f97401f
AlexFZR <abadragan@gmail.com>
2023-06-10 15:32:50 +0200

    16 passed, 3 fail

--- exercism/ISBN Verifier /main.go
+++ exercism/ISBN Verifier /main.go
@@ -15,11 +15,26 @@ func main() {
 
 func IsValidISBN(isbn string) bool {
 	trimmed := strings.Replace(isbn, "-", "", -1)
+	if len(trimmed) != 10 {
+		return false
+	}
+	var checkDigit int
+	if trimmed[9] == 88 {
+		checkDigit = 10
+	} else {
+		checkDigit = int(trimmed[9] - 48)
+	}
+
+	trimmed = trimmed[:9]
 
 	var ISBN int
 	for i := 0; i < len(trimmed); i++ {
+		if int(trimmed[i])-48 < 0 || int(trimmed[i])-48 > 9 {
+			return false
+		}
 		ISBN += (int(trimmed[0]) - 48) * (len(trimmed) - i)
 	}
+	ISBN += checkDigit
 	if ISBN%11 == 0 {
 		return true
 	}
