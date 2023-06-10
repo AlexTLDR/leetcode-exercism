3784abebd84eb53db2bd7f82033755c0f0b67050
AlexFZR <abadragan@gmail.com>
2023-06-10 13:12:49 +0200

    simplified the regexp

--- exercism/Word Count /main.go
+++ exercism/Word Count /main.go
@@ -16,7 +16,8 @@ type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
 	wordCount := make(map[string]int)
-	words := regexp.MustCompile("[a-zA-Z0-9]+(['][a-zA-Z0-9]+)?").FindAllString(phrase, -1)
+	words := regexp.MustCompile(`\w+('[A-z]+)?`).FindAllString(phrase, -1)
+	//words := regexp.MustCompile("[a-zA-Z0-9]+(['][a-zA-Z0-9]+)?").FindAllString(phrase, -1)
 
 	for _, w := range words {
 		wordCount[strings.ToLower(w)]++
