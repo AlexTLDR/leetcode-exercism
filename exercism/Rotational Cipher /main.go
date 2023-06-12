18f5164dc61ec6b96e4656503f3bdf7007d0f857
AlexFZR <abadragan@gmail.com>
2023-06-12 21:16:27 +0200

    Rotational Cipher initial commit

---
+++ exercism/Rotational Cipher /main.go
@@ -0,0 +1,13 @@
+// https://exercism.org/tracks/go/exercises/rotational-cipher
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(RotationalCipher("abcdefghijklmnopqrstuvwxyz"))
+}
+
+func RotationalCipher(plain string, shiftKey int) string {
+	panic("Please implement the RotationalCipher function")
+}
