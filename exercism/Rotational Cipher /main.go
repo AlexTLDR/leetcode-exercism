9f1c5d44d80600b3315e2bea06ab703aadd258e8
AlexFZR <abadragan@gmail.com>
2023-06-12 22:03:16 +0200

    declared alphabet map

--- exercism/Rotational Cipher /main.go
+++ exercism/Rotational Cipher /main.go
@@ -9,5 +9,40 @@ func main() {
 }
 
 func RotationalCipher(plain string, shiftKey int) string {
-	panic("Please implement the RotationalCipher function")
+	alphabet := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+		"d": 4,
+		"e": 5,
+		"f": 6,
+		"g": 7,
+		"h": 8,
+		"i": 9,
+		"j": 10,
+		"k": 11,
+		"l": 12,
+		"m": 13,
+		"n": 14,
+		"o": 15,
+		"p": 16,
+		"q": 17,
+		"r": 18,
+		"s": 19,
+		"t": 20,
+		"u": 21,
+		"v": 22,
+		"w": 23,
+		"x": 24,
+		"y": 25,
+		"z": 26,
+	}
+
+	for _, r := range plain {
+		for s, K := range alphabet {
+			if string(r) == s {
+
+			}
+		}
+	}
 }
