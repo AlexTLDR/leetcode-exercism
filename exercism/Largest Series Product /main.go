a43b26b4c44cef7762045f6434a170379715aabb
AlexFZR <abadragan@gmail.com>
2023-06-18 15:54:45 +0200

    5 passed, 8 failed

--- exercism/Largest Series Product /main.go
+++ exercism/Largest Series Product /main.go
@@ -9,5 +9,18 @@ func main() {
 }
 
 func LargestSeriesProduct(digits string, span int) (int64, error) {
-	panic("Please implement the LargestSeriesProduct function")
+	if len(digits) < span || span < 2 {
+		return 0, fmt.Errorf("invalid")
+	}
+	var max int
+	for i := 0; i < len(digits)-span+1; i++ {
+		var product int
+		for j := i; j < i+span; j++ {
+			product *= int(digits[j] - '0')
+		}
+		if product > max {
+			max = product
+		}
+	}
+	return int64(max), nil
 }
