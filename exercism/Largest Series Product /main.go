f78a6b8eeb3ea5d48452da1e3aa6db0730a7ff85
AlexFZR <abadragan@gmail.com>
2023-06-18 16:58:11 +0200

    2nd iteration

--- exercism/Largest Series Product /main.go
+++ exercism/Largest Series Product /main.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Println(LargestSeriesProduct("73167176531330624919225119674426574742355349194934", 3))
 }
 
-func LargestSeriesProduct(digits string, span int) (int64, error) {
+func LargestSeriesProduct2(digits string, span int) (int64, error) {
 	for _, v := range digits {
 		if v < '0' || v > '9' {
 			return 0, fmt.Errorf("input should only contain digits")
@@ -39,4 +39,28 @@ func LargestSeriesProduct(digits string, span int) (int64, error) {
 	}
 
 	return largestProduct, nil
+}
+
+func LargestSeriesProduct(digits string, span int) (int64, error) {
+	for _, v := range digits {
+		if v < '0' || v > '9' {
+			return 0, fmt.Errorf("input should only contain digits")
+		}
+
+	}
+	if span <= 0 || span > len(digits) {
+		return 0, fmt.Errorf("invalid span value")
+	}
+
+	var max int
+	for i := 0; i < len(digits)-span+1; i++ {
+		product := 1
+		for j := i; j < i+span; j++ {
+			product *= int(digits[j] - '0')
+		}
+		if product > max {
+			max = product
+		}
+	}
+	return int64(max), nil
 }
