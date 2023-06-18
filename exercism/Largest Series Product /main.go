6031f09b0111add965e6c6a4ca1aa58f9d50a6cf
AlexFZR <abadragan@gmail.com>
2023-06-18 15:40:35 +0200

    initial commit

---
+++ exercism/Largest Series Product /main.go
@@ -0,0 +1,13 @@
+// https://exercism.org/tracks/go/exercises/largest-series-product
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(LargestSeriesProduct("63915", 3))
+}
+
+func LargestSeriesProduct(digits string, span int) (int64, error) {
+	panic("Please implement the LargestSeriesProduct function")
+}
