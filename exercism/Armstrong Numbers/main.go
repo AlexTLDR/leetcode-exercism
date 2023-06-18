5c1bd11e3d0d0b4763540799d719be78cba5cd11
AlexFZR <abadragan@gmail.com>
2023-06-18 17:25:53 +0200

    Armstrong Numbers initial commit

---
+++ exercism/Armstrong Numbers/main.go
@@ -0,0 +1,13 @@
+// https://exercism.org/tracks/go/exercises/armstrong-numbers
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(IsNumber(153))
+}
+
+func IsNumber(n int) bool {
+	panic("Please implement the IsNumber function")
+}
