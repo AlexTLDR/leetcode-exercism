b9fda034cfd21b4daf6e2b4a89b0a1a8dac62d6c
AlexFZR <abadragan@gmail.com>
2023-06-19 11:26:42 +0200

    14 passed, 12 failed

--- exercism/Series /main.go
+++ exercism/Series /main.go
@@ -6,10 +6,17 @@ import "fmt"
 
 func main() {
 	fmt.Println(UnsafeFirst(3, "123456"))
+	fmt.Println(All(3, "1234567"))
 }
 
 func All(n int, s string) []string {
-	panic("Please implement the All function")
+	var rs []string
+
+	for i := 0; i < len(s)-n; i++ {
+		ss := s[i : i+n]
+		rs = append(rs, ss)
+	}
+	return rs
 }
 
 func UnsafeFirst(n int, s string) string {
