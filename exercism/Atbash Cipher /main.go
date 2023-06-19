7c5e115598a7f2f4644079389e40f3735faef7dc
AlexFZR <abadragan@gmail.com>
2023-06-19 11:50:41 +0200

    initial commit

---
+++ exercism/Atbash Cipher /main.go
@@ -0,0 +1,13 @@
+// https://exercism.org/tracks/go/exercises/atbash-cipher
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Atbash("x123 yes"))
+}
+
+func Atbash(s string) string {
+	panic("Please implement the Atbash function")
+}
