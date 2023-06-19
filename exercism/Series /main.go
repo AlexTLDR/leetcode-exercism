9f8894ce80f04c3a8f6d75e46d8d820be031b841
AlexFZR <abadragan@gmail.com>
2023-06-19 10:25:50 +0200

    initial commit

---
+++ exercism/Series /main.go
@@ -0,0 +1,15 @@
+// https://exercism.org/tracks/go/exercises/series
+
+package main
+
+func main() {
+
+}
+
+func All(n int, s string) []string {
+	panic("Please implement the All function")
+}
+
+func UnsafeFirst(n int, s string) string {
+	panic("Please implement the UnsafeFirst function")
+}
