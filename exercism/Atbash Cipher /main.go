2e1465017f9e891356fef3ca54012a21b42f1da3
AlexFZR <abadragan@gmail.com>
2023-06-21 15:03:54 +0200

    4 passed, 4 failed

--- exercism/Atbash Cipher /main.go
+++ exercism/Atbash Cipher /main.go
@@ -11,43 +11,74 @@ func main() {
 	fmt.Println(Atbash("x123 yes"))
 }
 
-func Atbash(s string) string {
-	var alphabetMap = map[string]string{
-		"a": "z",
-		"b": "y",
-		"c": "x",
-		"d": "w",
-		"e": "v",
-		"f": "u",
-		"g": "t",
-		"h": "s",
-		"i": "r",
-		"j": "q",
-		"k": "p",
-		"l": "o",
-		"m": "n",
-		"n": "m",
-		"o": "l",
-		"p": "k",
-		"q": "j",
-		"r": "i",
-		"s": "h",
-		"t": "g",
-		"u": "f",
-		"v": "e",
-		"w": "d",
-		"x": "c",
-		"y": "b",
-		"z": "a",
-	}
+// func Atbash(s string) string {
+// 	var alphabetMap = map[string]string{
+// 		"a": "z",
+// 		"b": "y",
+// 		"c": "x",
+// 		"d": "w",
+// 		"e": "v",
+// 		"f": "u",
+// 		"g": "t",
+// 		"h": "s",
+// 		"i": "r",
+// 		"j": "q",
+// 		"k": "p",
+// 		"l": "o",
+// 		"m": "n",
+// 		"n": "m",
+// 		"o": "l",
+// 		"p": "k",
+// 		"q": "j",
+// 		"r": "i",
+// 		"s": "h",
+// 		"t": "g",
+// 		"u": "f",
+// 		"v": "e",
+// 		"w": "d",
+// 		"x": "c",
+// 		"y": "b",
+// 		"z": "a",
+// 	}
 
-	for _, v := range s {
-		for w := range alphabetMap {
-			if string(v) == w {
-				s = strings.Replace(s, w, alphabetMap[w], 1)
-			}
+// 	for _, v := range s {
+// 		for w := range alphabetMap {
+// 			if strings.ToLower(string(v)) == w {
+// 				s = strings.Replace(s, w, alphabetMap[w], 1)
+// 			}
+// 		}
+// 	}
+// 	return s
+
+// }
+
+func Atbash(s string) string {
+	s = strings.ToLower(s)
+	cipherText := ""
+	groupedText := ""
+	count := 0
+
+	for _, char := range s {
+		if char >= 'a' && char <= 'z' {
+			cipherChar := 'z' - (char - 'a')
+			cipherText += string(cipherChar)
+		} else {
+			cipherText += string(char)
+		}
+
+		if count == 4 {
+			groupedText += cipherText + " "
+			cipherText = ""
+			count = 0
+		} else {
+			count++
 		}
 	}
-	return s
+
+	if cipherText != "" {
+		groupedText += cipherText
+	}
+
+	return groupedText
 
 }
