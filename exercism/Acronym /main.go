d83ed116ed1a4c4fc4c9662ec8eea4f078e76fea
AlexFZR <abadragan@gmail.com>
2023-06-21 18:15:57 +0200

    5 passed, 4 failed

--- exercism/Acronym /main.go
+++ exercism/Acronym /main.go
@@ -2,13 +2,23 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
 func main() {
 	fmt.Println(Abbreviate("Liquid-crystal display"))
 }
 
 func Abbreviate(s string) string {
-
-	return ""
+	var abbv string
+	abbv += string(s[0])
+	for i := 1; i < len(s); i++ {
+		if !unicode.IsLetter(rune(s[i])) {
+			abbv += string(s[i+1])
+		}
+	}
+	return strings.ToUpper(abbv)
 }
