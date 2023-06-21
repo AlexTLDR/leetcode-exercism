205fc7cf03bca6bd24711b79b3543ad0c34c5ead
AlexFZR <abadragan@gmail.com>
2023-06-21 17:23:07 +0200

    passed all the tests

--- exercism/Atbash Cipher /main.go
+++ exercism/Atbash Cipher /main.go
@@ -11,74 +11,27 @@ func main() {
 	fmt.Println(Atbash("x123 yes"))
 }
 
-// func Atbash(s string) string {
-// 	var alphabetMap = map[string]string{
-// 		"a": "z",
-// 		"b": "y",
-// 		"c": "x",
-// 		"d": "w",
-// 		"e": "v",
-// 		"f": "u",
-// 		"g": "t",
-// 		"h": "s",
-// 		"i": "r",
-// 		"j": "q",
-// 		"k": "p",
-// 		"l": "o",
-// 		"m": "n",
-// 		"n": "m",
-// 		"o": "l",
-// 		"p": "k",
-// 		"q": "j",
-// 		"r": "i",
-// 		"s": "h",
-// 		"t": "g",
-// 		"u": "f",
-// 		"v": "e",
-// 		"w": "d",
-// 		"x": "c",
-// 		"y": "b",
-// 		"z": "a",
-// 	}
-
-// 	for _, v := range s {
-// 		for w := range alphabetMap {
-// 			if strings.ToLower(string(v)) == w {
-// 				s = strings.Replace(s, w, alphabetMap[w], 1)
-// 			}
-// 		}
-// 	}
-// 	return s
-
-// }
-
 func Atbash(s string) string {
 	s = strings.ToLower(s)
-	cipherText := ""
+	cipherText := strings.Map(func(r rune) rune {
+		switch {
+		case r >= 'a' && r <= 'z':
+			return 'z' - (r - 'a')
+		case r >= '0' && r <= '9':
+			return r
+		default:
+			return -1
+		}
+	}, s)
+
+	groupSize := 5
 	groupedText := ""
-	count := 0
-
-	for _, char := range s {
-		if char >= 'a' && char <= 'z' {
-			cipherChar := 'z' - (char - 'a')
-			cipherText += string(cipherChar)
-		} else {
-			cipherText += string(char)
+	for i, char := range cipherText {
+		groupedText += string(char)
+		if (i+1)%groupSize == 0 && i != len(cipherText)-1 {
+			groupedText += " "
 		}
-
-		if count == 4 {
-			groupedText += cipherText + " "
-			cipherText = ""
-			count = 0
-		} else {
-			count++
-		}
-	}
-
-	if cipherText != "" {
-		groupedText += cipherText
 	}
 
 	return groupedText
-
 }
