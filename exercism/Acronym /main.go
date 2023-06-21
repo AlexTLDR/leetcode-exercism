5c09c59e80cda66c53c2971c031bbc20b08ed837
AlexFZR <abadragan@gmail.com>
2023-06-21 18:42:25 +0200

    8 passed, 1 failed

--- exercism/Acronym /main.go
+++ exercism/Acronym /main.go
@@ -16,8 +16,8 @@ func Abbreviate(s string) string {
 	var abbv string
 	abbv += string(s[0])
 	for i := 1; i < len(s); i++ {
-		if !unicode.IsLetter(rune(s[i])) {
-			abbv += string(s[i+1])
+		if !unicode.IsLetter(rune(s[i-1])) && unicode.IsLetter(rune(s[i])) {
+			abbv += string(s[i])
 		}
 	}
 	return strings.ToUpper(abbv)
