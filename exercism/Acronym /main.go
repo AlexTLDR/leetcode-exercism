74bd71aa3539f9745bef628fb864331a31a326cb
AlexFZR <abadragan@gmail.com>
2023-06-21 17:53:31 +0200

    initial commit

---
+++ exercism/Acronym /main.go
@@ -0,0 +1,14 @@
+// https://exercism.org/tracks/go/exercises/acronym
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Abbreviate("Liquid-crystal display"))
+}
+
+func Abbreviate(s string) string {
+
+	return ""
+}
