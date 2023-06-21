0c53c43e96f613b095f16c1e8b1d47bd7cbe2b8a
AlexFZR <abadragan@gmail.com>
2023-06-21 19:16:26 +0200

    trying to fix the ' in halley's

--- exercism/Acronym /main.go
+++ exercism/Acronym /main.go
@@ -9,15 +9,18 @@ import (
 )
 
 func main() {
-	fmt.Println(Abbreviate("Liquid-crystal display"))
+	fmt.Println(Abbreviate("Halley's Comet"))
 }
 
 func Abbreviate(s string) string {
 	var abbv string
 	abbv += string(s[0])
 	for i := 1; i < len(s); i++ {
+		if s[i] == 39 {
+		}
 		if !unicode.IsLetter(rune(s[i-1])) && unicode.IsLetter(rune(s[i])) {
 			abbv += string(s[i])
+
 		}
 	}
 	return strings.ToUpper(abbv)
