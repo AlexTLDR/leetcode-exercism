df97c65d65764474497ba7ce9501a69237d14dc4
AlexFZR <abadragan@gmail.com>
2023-06-22 15:50:00 +0200

    passed all the tests

--- exercism/Acronym /main.go
+++ exercism/Acronym /main.go
@@ -15,8 +15,11 @@ func main() {
 func Abbreviate(s string) string {
 	var abbv string
 	abbv += string(s[0])
+
 	for i := 1; i < len(s); i++ {
 		if s[i] == 39 {
+			i++
+			i++
 		}
 		if !unicode.IsLetter(rune(s[i-1])) && unicode.IsLetter(rune(s[i])) {
 			abbv += string(s[i])
