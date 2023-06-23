a501e171592d1a6064065c9c7eddffa87ad9976b
AlexFZR <abadragan@gmail.com>
2023-06-23 16:15:22 +0200

    initial commit

---
+++ exercism/Bottle Song /main.go
@@ -0,0 +1,11 @@
+// https://exercism.org/tracks/go/exercises/bottle-song
+
+package main
+
+func main() {
+
+}
+
+func Recite(startBottles, takeDown int) []string {
+	panic("Please implement the Recite function")
+}
