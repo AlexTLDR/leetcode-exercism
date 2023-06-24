f18a6de3467ceca69cf592fb335b4f78a0a3e756
AlexFZR <abadragan@gmail.com>
2023-06-24 16:43:47 +0200

    passed all the tests

--- exercism/Prime Factors /main.go
+++ exercism/Prime Factors /main.go
@@ -5,9 +5,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Factors(630))
+	fmt.Println(Factors(60))
 }
 
 func Factors(n int64) []int64 {
-
+	var factors []int64
+	var i int64 = 2
+	for n > 1 {
+		if n%i == 0 {
+			factors = append(factors, i)
+			n /= i
+		} else {
+			i++
+		}
+	}
+	return factors
 }
