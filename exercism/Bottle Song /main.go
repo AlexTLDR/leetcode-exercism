5464bb18084867cea57e1c6e88807713279cf9f6
AlexFZR <abadragan@gmail.com>
2023-06-24 15:03:10 +0200

    5 passed, 2 failed

--- exercism/Bottle Song /main.go
+++ exercism/Bottle Song /main.go
@@ -8,13 +8,16 @@ import (
 )
 
 func main() {
-	fmt.Println(Recite(3, 1))
+	fmt.Println(Recite(3, 3))
 }
 
 func Recite(startBottles, takeDown int) []string {
 	var song []string
 	stopBottles := startBottles - takeDown
 	for i := startBottles; i > stopBottles; i-- {
+		if i < startBottles {
+			song = append(song, "")
+		}
 		song = Verse(song, i, takeDown)
 	}
 	return song
@@ -33,8 +36,8 @@ func Verse(song []string, startBottles, takeDown int) []string {
 	}
 
 	line1 := fmt.Sprintf("%s green bottle%s hanging on the wall,", strings.Title(numbers[startBottles]), plural1)
-	line2 := fmt.Sprintf("And if %s green bottle should accidentally fall,", numbers[takeDown])
-	line3 := fmt.Sprintf("There'll be %s green bottle%s hanging on the wall.", numbers[startBottles-takeDown], plural3)
+	line2 := fmt.Sprintf("And if %s green bottle should accidentally fall,", "one")
+	line3 := fmt.Sprintf("There'll be %s green bottle%s hanging on the wall.", numbers[startBottles-1], plural3)
 	song = append(song, line1, line1, line2, line3)
 	return song
 }
