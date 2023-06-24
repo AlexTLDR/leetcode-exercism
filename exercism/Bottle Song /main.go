21dbdf0d2b02622d3ef83cdc1292c4e3840b214d
AlexFZR <abadragan@gmail.com>
2023-06-24 12:41:16 +0200

    first structure

--- exercism/Bottle Song /main.go
+++ exercism/Bottle Song /main.go
@@ -2,10 +2,28 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func main() {
+	fmt.Println(Recite(10, 1))
 }
 
 func Recite(startBottles, takeDown int) []string {
-	panic("Please implement the Recite function")
+	var song []string
+	for i := 0; i < takeDown; i++ {
+		song = Verse(song, startBottles, takeDown)
+
+	}
+	return song
+}
+
+func Verse(song []string, startBottles, takeDown int) []string {
+	song = append(song, strconv.Itoa(startBottles), "green bottles hanging on the wall,\n")
+	song = append(song, strconv.Itoa(startBottles), "green bottles hanging on the wall,\n")
+	song = append(song, "And if", strconv.Itoa(takeDown), "green bottle should accidentally fall,\n")
+	return song
+
 }
