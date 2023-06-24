b35992d8412a1f08b21bc221bc7db2c909bffff2
AlexFZR <abadragan@gmail.com>
2023-06-24 16:18:52 +0200

    initial commit

---
+++ exercism/Simple Cipher/main.go
@@ -0,0 +1,35 @@
+// https://exercism.org/tracks/go/exercises/simple-cipher
+
+package main
+
+func main() {
+
+}
+
+func NewCaesar() Cipher {
+	panic("Please implement the NewCaesar function")
+}
+
+func NewShift(distance int) Cipher {
+	panic("Please implement the NewShift function")
+}
+
+func (c shift) Encode(input string) string {
+	panic("Please implement the Encode function")
+}
+
+func (c shift) Decode(input string) string {
+	panic("Please implement the Decode function")
+}
+
+func NewVigenere(key string) Cipher {
+	panic("Please implement the NewVigenere function")
+}
+
+func (v vigenere) Encode(input string) string {
+	panic("Please implement the Encode function")
+}
+
+func (v vigenere) Decode(input string) string {
+	panic("Please implement the Decode function")
+}
