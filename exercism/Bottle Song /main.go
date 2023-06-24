386e21dd949d3983a76d81497445b5e631873e78
AlexFZR <abadragan@gmail.com>
2023-06-24 12:58:47 +0200

    changed int to strings

--- exercism/Bottle Song /main.go
+++ exercism/Bottle Song /main.go
@@ -4,7 +4,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
@@ -13,17 +12,28 @@ func main() {
 
 func Recite(startBottles, takeDown int) []string {
 	var song []string
-	for i := 0; i < takeDown; i++ {
-		song = Verse(song, startBottles, takeDown)
-
+	for i := startBottles; i > 0; i-- {
+		song = Verse(song, i, takeDown)
 	}
 	return song
 }
 
 func Verse(song []string, startBottles, takeDown int) []string {
-	song = append(song, strconv.Itoa(startBottles), "green bottles hanging on the wall,\n")
-	song = append(song, strconv.Itoa(startBottles), "green bottles hanging on the wall,\n")
-	song = append(song, "And if", strconv.Itoa(takeDown), "green bottle should accidentally fall,\n")
+	numbers := []string{"no more", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"}
+	if startBottles < 1 {
+		return song
+	}
+	if startBottles == 1 {
+		song = append(song, numbers[startBottles], "green bottle hanging on the wall,\n")
+		song = append(song, numbers[startBottles], "green bottle hanging on the wall,\n")
+		song = append(song, "And if", numbers[takeDown], "green bottle should accidentally fall,\n")
+		song = append(song, "There'll be no more bottles hanging on the wall.\n")
+		return song
+	}
+	song = append(song, numbers[startBottles], "green bottles hanging on the wall,\n")
+	song = append(song, numbers[startBottles], "green bottles hanging on the wall,\n")
+	song = append(song, "And if", numbers[takeDown], "green bottle should accidentally fall,\n")
+	song = append(song, "There'll be", numbers[startBottles-takeDown], "green bottles hanging on the wall.\n\n")
 	return song
 
 }
