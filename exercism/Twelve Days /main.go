40a07f6180eaeafa4b19c835c6ffef0593f8ac24
AlexFZR <abadragan@gmail.com>
2023-06-25 12:54:27 +0200

    initial commit

---
+++ exercism/Twelve Days /main.go
@@ -0,0 +1,15 @@
+// https://exercism.org/tracks/go/exercises/twelve-days
+
+package main
+
+func main() {
+
+}
+
+func Verse(i int) string {
+	panic("Please implement the Verse function")
+}
+
+func Song() string {
+	panic("Please implement the Song function")
+}
