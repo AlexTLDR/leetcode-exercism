e31eaf2766587a5e62b17bbb17c57dd75f3e9fa0
AlexFZR <abadragan@gmail.com>
2023-06-25 13:38:03 +0200

    implemented the Verse function

--- exercism/Twelve Days /main.go
+++ exercism/Twelve Days /main.go
@@ -2,6 +2,11 @@
 
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
 
 }
@@ -9,8 +14,14 @@ func main() {
 var days = []string{"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"}
 var gifts = []string{"a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens", "four Calling Birds", "five Gold Rings", "six Geese-a-Laying", "seven Swans-a-Swimming", "eight Maids-a-Milking", "nine Ladies Dancing", "ten Lords-a-Leaping", "eleven Pipers Piping", "twelve Drummers Drumming"}
 
+const repeat = "On the %s day of Christmas my true love gave to me: %s"
+
 func Verse(i int) string {
-	panic("Please implement the Verse function")
+	if i < 1 || i > 12 {
+		return ""
+	}
+
+	return fmt.Sprintf(repeat, days[i-1], strings.Join(gifts[0:i], ", "))
 }
 
 func Song() string {
