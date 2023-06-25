3b06124d9b856a97a960397ae697ba5e17f3625d
AlexFZR <abadragan@gmail.com>
2023-06-25 13:32:57 +0200

    days and gifts lists

--- exercism/Twelve Days /main.go
+++ exercism/Twelve Days /main.go
@@ -6,6 +6,9 @@ func main() {
 
 }
 
+var days = []string{"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"}
+var gifts = []string{"a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens", "four Calling Birds", "five Gold Rings", "six Geese-a-Laying", "seven Swans-a-Swimming", "eight Maids-a-Milking", "nine Ladies Dancing", "ten Lords-a-Leaping", "eleven Pipers Piping", "twelve Drummers Drumming"}
+
 func Verse(i int) string {
 	panic("Please implement the Verse function")
 }
