6839c47b054ace2228baef5e67e3baecdaa66c70
AlexFZR <abadragan@gmail.com>
2023-07-01 11:00:58 +0200

    initial commit

---
+++ exercism/Sum of Multiples /main.go
@@ -0,0 +1,13 @@
+// https://exercism.org/tracks/go/exercises/sum-of-multiples
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(SumMultiples(20, 3, 5))
+}
+
+func SumMultiples(limit int, divisors ...int) int {
+	panic("Please implement the SumMultiples function")
+}
