2d0522d3ebe6ec1d09efc33d7fbd392405092d6a
AlexFZR <abadragan@gmail.com>
2023-07-01 11:10:01 +0200

    passed all the tests

--- exercism/Sum of Multiples /main.go
+++ exercism/Sum of Multiples /main.go
@@ -12,6 +12,9 @@ func SumMultiples(limit int, divisors ...int) int {
 	var sum int
 	for i := 1; i < limit; i++ {
 		for _, divisor := range divisors {
+			if divisor == 0 {
+				continue
+			}
 			if i%divisor == 0 {
 				sum += i
 				break
