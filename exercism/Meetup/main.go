1d725fab468de11edcdab354ed535d1591210729
AlexTLDR <alex.badragan@protonmail.com>
2023-08-17 13:11:03 +0200

    meetup initial commit

---
+++ exercism/Meetup/main.go
@@ -0,0 +1,13 @@
+// https://exercism.org/tracks/go/exercises/meetup
+
+package main
+
+import "time"
+
+func main() {
+
+}
+
+func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
+	panic("Please implement the Day function")
+}
