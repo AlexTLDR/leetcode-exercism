43defda4886ef3a21603b496c7d693477b8c230f
AlexTLDR <alex.badragan@protonmail.com>
2023-08-17 13:01:48 +0200

    passed all the tests

--- exercism/Complex Numbers/main.go
+++ exercism/Complex Numbers/main.go
@@ -1,43 +1,73 @@
-// https://exercism.org/tracks/go/exercises/complex-numbers
-
 package complexnumbers
 
+import "math"
+
+type Number struct {
+	real      float64
+	imaginary float64
+}
+
 func (n Number) Real() float64 {
-	panic("Please implement the Real method")
+	return n.real
 }
 
 func (n Number) Imaginary() float64 {
-	panic("Please implement the Imaginary method")
+	return n.imaginary
 }
 
 func (n1 Number) Add(n2 Number) Number {
-	panic("Please implement the Add method")
+	return Number{
+		real:      n1.real + n2.real,
+		imaginary: n1.imaginary + n2.imaginary,
+	}
 }
 
 func (n1 Number) Subtract(n2 Number) Number {
-	panic("Please implement the Subtract method")
+	return Number{
+		real:      n1.real - n2.real,
+		imaginary: n1.imaginary - n2.imaginary,
+	}
 }
 
 func (n1 Number) Multiply(n2 Number) Number {
-	panic("Please implement the Multiply method")
+	r1, i1, r2, i2 := n1.real, n1.imaginary, n2.real, n2.imaginary
+	return Number{
+		real:      r1*r2 - i1*i2,
+		imaginary: i1*r2 + r1*i2,
+	}
 }
 
 func (n Number) Times(factor float64) Number {
-	panic("Please implement the Times method")
+	return Number{
+		real:      factor * n.real,
+		imaginary: factor * n.imaginary,
+	}
 }
 
 func (n1 Number) Divide(n2 Number) Number {
-	panic("Please implement the Divide method")
+	r1, i1, r2, i2 := n1.real, n1.imaginary, n2.real, n2.imaginary
+	divisor := r2*r2 + i2*i2
+	return Number{
+		real:      (r1*r2 + i1*i2) / divisor,
+		imaginary: (i1*r2 - r1*i2) / divisor,
+	}
 }
 
 func (n Number) Conjugate() Number {
-	panic("Please implement the Conjugate method")
+	return Number{
+		real:      n.real,
+		imaginary: -n.imaginary,
+	}
 }
 
 func (n Number) Abs() float64 {
-	panic("Please implement the Abs method")
+	return math.Sqrt(n.real*n.real + n.imaginary*n.imaginary)
 }
 
 func (n Number) Exp() Number {
-	panic("Please implement the Exp method")
+	expReal := math.Exp(n.real)
+	return Number{
+		real:      expReal * math.Cos(n.imaginary),
+		imaginary: expReal * math.Sin(n.imaginary),
+	}
 }
