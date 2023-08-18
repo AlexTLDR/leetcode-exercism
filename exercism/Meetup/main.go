082278ecb645cf26524f709dfc40d423a5d1cbb1
AlexTLDR <alex.badragan@protonmail.com>
2023-08-18 10:30:39 +0200

    completed func Day

--- exercism/Meetup/main.go
+++ exercism/Meetup/main.go
@@ -4,10 +4,32 @@ package main
 
 import "time"
 
-func main() {
+type WeekSchedule int
 
-}
+const (
+	First  WeekSchedule = 1
+	Second WeekSchedule = 2
+	Third  WeekSchedule = 3
+	Fourth WeekSchedule = 4
+	Last   WeekSchedule = 5
+	Teenth WeekSchedule = 6
+)
 
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
-	panic("Please implement the Day function")
+	var day int
+	switch wSched {
+	case First:
+		day = 1
+	case Second:
+		day = 8
+	case Third:
+		day = 15
+	case Fourth:
+		day = 22
+	case Last:
+		day = lastDay(month, year)
+	case Teenth:
+		day = 13
+	}
+	return day + int(wDay-time.Date(year, month, 1, 0, 0, 0, 0, time.UTC).Weekday()+7)%7
 }
