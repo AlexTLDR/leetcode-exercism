bee9009248e5118ef9d0a15ccbbaa8382e29a4e2
AlexTLDR <alex.badragan@protonmail.com>
2023-08-21 16:34:38 +0200

    passed all the tests

--- exercism/Rust track/Leap/main.rs
+++ exercism/Rust track/Leap/main.rs
@@ -1,5 +1,14 @@
 // https://exercism.org/tracks/rust/exercises/leap
 
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {year} is a leap year")
+    (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+}
+
+fn main() {
+    let year = 2024; // Replace with the year you want to check
+    if is_leap_year(year) {
+        println!("{} is a leap year.", year);
+    } else {
+        println!("{} is not a leap year.", year);
+    }
 }
