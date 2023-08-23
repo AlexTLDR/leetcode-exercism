6ac312d48d6ea7a2923c5d90af707991c0ab780a
AlexTLDR <alex.badragan@protonmail.com>
2023-08-23 11:58:46 +0200

    5 tests passed, 3 failed

--- exercism/Rust track/Collatz Conjecture/main.rs
+++ exercism/Rust track/Collatz Conjecture/main.rs
@@ -1,7 +1,29 @@
 // https://exercism.org/tracks/rust/exercises/collatz-conjecture
 
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {n}"
-    )
+    if n == 0 {
+        return None;
+    }
+
+    let mut steps = 0;
+    let mut num = n;
+
+    while num != 1 {
+        if num % 2 == 0 {
+            num /= 2;
+        } else {
+            num = 3 * num + 1;
+        }
+        steps += 1;
+    }
+
+    Some(steps)
+}
+
+fn main() {
+    let n = 27; // Replace with the number you want to test
+    match collatz(n) {
+        Some(steps) => println!("Number of steps to reach 1: {}", steps),
+        None => println!("Invalid input"),
+    }
 }
