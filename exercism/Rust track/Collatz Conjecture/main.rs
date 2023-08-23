4be316c5f78874a031080aab2ca352182c04ad9a
AlexTLDR <alex.badragan@protonmail.com>
2023-08-23 11:53:12 +0200

    added the link to the exercise

--- exercism/Rust track/Collatz Conjecture/main.rs
+++ exercism/Rust track/Collatz Conjecture/main.rs
@@ -1,3 +1,5 @@
+// https://exercism.org/tracks/rust/exercises/collatz-conjecture
+
 pub fn collatz(n: u64) -> Option<u64> {
     unimplemented!(
         "return Some(x) where x is the number of steps required to reach 1 starting with {n}"
