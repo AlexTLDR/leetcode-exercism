26311dbdedbc61a4dff74c158fb54b2e3c36f985
AlexFZR <abadragan@gmail.com>
2023-10-17 18:47:59 +0200

    Kindergarten Garden initial commit

---
+++ exercism/Go track/Kindergarten Garden /main.go
@@ -0,0 +1,11 @@
+// https://exercism.org/tracks/go/exercises/kindergarten-garden
+
+package main
+
+func NewGarden(diagram string, children []string) (*Garden, error) {
+	panic("Please implement the NewGarden function")
+}
+
+func (g *Garden) Plants(child string) ([]string, bool) {
+	panic("Please implement the Plants function")
+}
