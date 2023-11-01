5541c3c3c4320e67bf608efbe4b2bee8b2445e8f
AlexTLDR <alex.badragan@protonmail.com>
2023-11-01 18:42:10 +0100

    passed all the tests

--- exercism/Go track/Binary Search /main.go
+++ exercism/Go track/Binary Search /main.go
@@ -3,5 +3,20 @@
 package main
 
 func SearchInts(list []int, key int) int {
-	panic("Please implement the SearchInts function")
+	low := 0
+	high := len(list) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		if list[mid] < key {
+			low = mid + 1
+		} else if list[mid] > key {
+			high = mid - 1
+		} else {
+			return mid
+		}
+	}
+
+	return -1
 }
