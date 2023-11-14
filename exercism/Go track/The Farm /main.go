415807ea61cc8c04e0e3a76dfa8228be1f9b0b0e
AlexTLDR <alex.badragan@protonmail.com>
2023-11-14 21:19:19 +0100

    5 tests passed,  6 failed

--- exercism/Go track/The Farm /main.go
+++ exercism/Go track/The Farm /main.go
@@ -11,25 +11,48 @@ type FodderCalculator interface {
 	FatteningFactor() (float64, error)
 }
 
-func DivideFood(fodderCalculator FodderCalculator, numCows int) (float64, error) {
-	totalFodder, err := fodderCalculator.FodderAmount(numCows)
+func DivideFood(calculator FodderCalculator, numCows int) (float64, error) {
+	total, err := calculator.FodderAmount(numCows)
 	if err != nil {
-		return 0, err
+		return 0, fmt.Errorf("error calculating total fodder: %w", err)
 	}
 
-	factor, err := fodderCalculator.FatteningFactor()
+	factor, err := calculator.FatteningFactor()
 	if err != nil {
-		return 0, err
+		return 0, fmt.Errorf("error retrieving fattening factor: %w", err)
 	}
 
-	foodPerCow := totalFodder / float64(numCows) * factor
-	return foodPerCow, nil
+	return total / float64(numCows) * factor, nil
 }
 
-func ValidateInputAndDivideFood(fodderCalculator FodderCalculator, numCows int) (float64, error) {
+func ValidateInputAndDivideFood(calculator FodderCalculator, numCows int) (float64, error) {
 	if numCows <= 0 {
-		return 0, fmt.Errorf("invalid number of cows")
+		return 0, NewInvalidCowsError(numCows, "invalid number of cows")
 	}
 
-	return DivideFood(fodderCalculator, numCows)
+	return DivideFood(calculator, numCows)
+}
+
+type InvalidCowsError struct {
+	num int
+	msg string
+}
+
+func (err *InvalidCowsError) Error() string {
+	return fmt.Sprintf("%d cows are invalid: %s", err.num, err.msg)
+}
+
+func NewInvalidCowsError(num int, msg string) *InvalidCowsError {
+	return &InvalidCowsError{num, msg}
+}
+
+func ValidateNumberOfCows(num int) error {
+	switch {
+	case num < 0:
+		return NewInvalidCowsError(num, "there are no negative cows")
+	case num == 0:
+		return NewInvalidCowsError(num, "no cows don't need food")
+	default:
+		return nil
+	}
 }
